22b87aec017addb69193469656a0a7a52e63d0b7
nicolocarpignoli <carpo23@gmail.com>
2026-05-05 11:43:24 +0200

    new speech package

--- src/voices/id.ts
+++ src/voices/id.ts
@@ -3,8 +3,8 @@ import type { Voice } from '../Voice';
 const data: Voice[] = [
     ['Microsoft Gadis Online (Natural) - Indonesian (Indonesia)', 'id-ID', 'female', 4],
     ['Microsoft Ardi Online (Natural) - Indonesian (Indonesia)', 'id-ID', 'male', 4],
-    ['Damayanti', 'id-ID', 'female', 2],
     ['Google Bahasa Indonesia', 'id-ID', 'female', 3],
+    ['Damayanti', 'id-ID', 'female', 2],
     ['Microsoft Andika - Indonesian (Indonesia)', 'id-ID', 'female', 2],
     ['Google Bahasa Indonesia 1 (Natural)', 'id-ID', 'female', 3],
     ['Google Bahasa Indonesia 2 (Natural)', 'id-ID', 'female', 3],
